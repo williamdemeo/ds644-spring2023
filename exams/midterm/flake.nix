e6bcda8d8dddafaf262d348f32ceddd78c7ed45d
William DeMeo <williamdemeo@gmail.com>
2023-03-27 01:21:24 -0400

    .

---
+++ exams/midterm/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "DS 644 Midterm Exam";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib; eachSystem allSystems (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-minimal latex-bin latexmk;
+      };
+    in rec {
+      packages = {
+        document = pkgs.stdenvNoCC.mkDerivation rec {
+          name = "ds644-midterm-exam";
+          src = self;
+          buildInputs = [ pkgs.coreutils tex ];
+          phases = ["unpackPhase" "buildPhase" "installPhase"];
+          buildPhase = ''
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+            mkdir -p .cache/texmf-var
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              latexmk -interaction=nonstopmode -pdf -lualatex \
+              midterm.tex
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp midterm.pdf $out/
+          '';
+        };
+      };
+      defaultPackage = packages.document;
+    });
+}
