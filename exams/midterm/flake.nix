cffaa05e0d795cc75572be732b2cf9ef0403f10e
William DeMeo <williamdemeo@gmail.com>
2023-03-27 01:34:37 -0400

    .

--- exams/midterm/flake.nix
+++ exams/midterm/flake.nix
@@ -9,7 +9,7 @@
     let
       pkgs = nixpkgs.legacyPackages.${system};
       tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-minimal latex-bin latexmk exam geometry amsmath amstex amscls;
+          inherit (pkgs.texlive) scheme-minimal latex-bin latexmk exam geometry amsmath amstex amscls mathtools;
       };
     in rec {
       packages = {
