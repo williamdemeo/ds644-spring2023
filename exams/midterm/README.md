dd01cb6fcdb35c76984d4f6af22e9d8f40a68f8e
William DeMeo <williamdemeo@gmail.com>
2023-04-09 03:57:16 -0400

    initial commit of new exam nix project template

---
+++ exams/midterm/README.md
@@ -0,0 +1,70 @@
+## Leixb-barcadoc-template
+
+The original source for the files in this directory were obtained from
+
+https://github.com/Leixb/latex-template
+
+**Instructions**.
+
+At the command line, navigate to the Leixb-barcadoc-template directory and 
+enter the following commands: `nix flake lock` and `nix build`.
+
+If the document generation is successful, the resulting pdf will
+reside in the `Leixb-barcadoc-template/result` directory.
+
+```
+cd Leixb-barcadoc-template
+nix flake lock
+nix build
+```
+
+The contents of the original `Leixb/latex-template/README.md` file follows below this line.
+
+------------------------------
+
+LaTeX flake template
+====================
+
+`LaTeX` template that I use as a starting point for all my documents. It is
+structured in multiple files ordered numerically. To compile simply
+run `latexmk`.
+
+Flakes
+------
+
+There is no need to use `flakes`, this template can be used with any working
+`LaTeX` installation, but `flakes` allow for build reproducibility and a
+standardized environment across all collaborators. This means that all the
+`LaTeX` dependencies, `pygmentize` and all other dependencies are managed with
+`nix`, allowing to compile the document without working about managing the
+dependencies in different environments.
+
+This makes it very easy to integrate the document deployment with [github
+actions](https://github.com/marketplace/actions/install-nix#usage-with-flakes),
+any other CI or [creating containers](https://nix.dev/tutorials/building-and-running-docker-images)
+to build the document. Using, binary caches with `cachix` can speed up the
+compilation significantly by caching all LaTeX packages.
+
+To build the current in the current directory run:
+
+```
+nix build
+```
+
+Or even compile the document without even cloning it before hand with:
+
+```
+nix build github:leixb/latex-template
+```
+
+To get a shell with all the dependencies available:
+
+```
+nix develop
+```
+
+Note that you can use [direnv](https://github.com/direnv/direnv)
+to have a controlled development environment for the document integrated
+with your shell.
+
+(All this commands are using nix 2.4 with flake support)
--- exams/midterm/flake.nix
+++ exams/midterm/flake.nix
@@ -1,36 +1,80 @@
 {
-  description = "DS 644 Midterm Exam";
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
-    flake-utils.url = github:numtide/flake-utils;
-  };
+  description = "Build LaTeX document with minted";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
   outputs = { self, nixpkgs, flake-utils }:
-    with flake-utils.lib; eachSystem allSystems (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-minimal latex-bin latexmk exam geometry amsmath amstex amscls mathtools;
+    {
+      templates.document = {
+        path = ./.;
+        description = "LaTeX document with minted support";
       };
-    in rec {
-      packages = {
-        document = pkgs.stdenvNoCC.mkDerivation rec {
-          name = "ds644-midterm-exam";
-          src = self;
-          buildInputs = [ pkgs.coreutils tex ];
-          phases = ["unpackPhase" "buildPhase" "installPhase"];
-          buildPhase = ''
-            export PATH="${pkgs.lib.makeBinPath buildInputs}";
-            mkdir -p .cache/texmf-var
-            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-              latexmk -interaction=nonstopmode -pdf -lualatex \
-              ./exams/midterm/midterm.tex
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp midterm.pdf $out/
-          '';
+
+      lib.latexmk = import ./build-document.nix;
+
+      defaultTemplate = self.templates.document;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        latex-packages = with pkgs; [
+          (texlive.combine {
+            inherit (texlive)
+              scheme-medium
+              exam
+              framed
+              titlesec
+              cleveref
+              multirow
+              wrapfig
+              tabu
+              threeparttable
+              threeparttablex
+              makecell
+              environ
+              # biblatex
+              # biber
+              fvextra
+              upquote
+              catchfile
+              xstring
+              csquotes
+              minted
+              dejavu
+              comment
+              footmisc
+              xltabular
+              ltablex
+              background
+              everypage
+              ;
+          })
+          which
+          python39Packages.pygments
+        ];
+
+        dev-packages = with pkgs; [
+          texlab
+          zathura
+          wmctrl
+        ];
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs = [ latex-packages dev-packages ];
         };
-      };
-      defaultPackage = packages.document;
-    });
+        
+        packages = flake-utils.lib.flattenTree {
+          document = import ./build-document.nix {
+            inherit pkgs;
+            texlive = latex-packages;
+            shellEscape = true;
+            minted = true;
+            SOURCE_DATE_EPOCH = toString self.lastModified;
+          };
+        };
+
+        defaultPackage = packages.document;
+      }
+    );
 }
