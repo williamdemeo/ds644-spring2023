bd14eb363e3d1ac447cf49f83be0d6aa9df7075b
William DeMeo <williamdemeo@gmail.com>
2023-05-10 17:00:05 -0400

    .

--- exams/midterm/.latexmkrc
+++
@@ -1,3 +0,0 @@
-$pdf_mode = 4;
-@default_files = ('000-main.tex');
-$lualatex = "lualatex %O -shell-escape %S";
--- exams/midterm/flake.nix
+++ exams/midterm/flake.nix
@@ -1,55 +1,35 @@
 {
-  description = "Build LaTeX document with minted";
-
-#  inputs.flake-utils.url = "github:numtide/flake-utils";
+  description = "DS 644 Midterm Exam";
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-21.05;
     flake-utils.url = github:numtide/flake-utils;
   };
-
   outputs = { self, nixpkgs, flake-utils }:
     with flake-utils.lib; eachSystem allSystems (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
       tex = pkgs.texlive.combine {
           inherit (pkgs.texlive)
-            #scheme-minimal
             scheme-medium
             latex-bin
             latexmk
             exam
-            framed
-            titlesec
-            cleveref
-            multirow
-            wrapfig
-            tabu
-            threeparttable
-            threeparttablex
-            makecell
-            environ
-              # biblatex
-              # biber
-            fvextra
-            upquote
-            catchfile
-            xstring
-            csquotes
-            minted
-            dejavu
+            geometry
+            amsmath
+            amstex
+            amscls
+            mathtools
             comment
-            footmisc
-            xltabular
-            ltablex
+            mathalpha
+            bbold
+            newunicodechar
             background
-            everypage
-            pygmentex
-            ;
+            everypage;
       };
     in rec {
       packages = {
         document = pkgs.stdenvNoCC.mkDerivation rec {
-          name = "latex-demo-document";
+          name = "ds644-midterm-exam";
           src = self;
           buildInputs = [ pkgs.coreutils tex ];
           phases = ["unpackPhase" "buildPhase" "installPhase"];
@@ -57,91 +37,18 @@
             export PATH="${pkgs.lib.makeBinPath buildInputs}";
             mkdir -p .cache/texmf-var
             env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-              latexmk -interaction=nonstopmode -pdf -lualatex \
-              exams/midterm/000-main.tex
+              latexmk -interaction=nonstopmode -pdf -lualatex -f \
+              templates/exams/midterm-example2/midterm.tex
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              latexmk -interaction=nonstopmode -pdf -lualatex -f \
+              templates/exams/midterm-example2/midterm-SOLUTIONS.tex
           '';
           installPhase = ''
             mkdir -p $out
-            cp document.pdf $out/
+            cp ./exams/midterm/midterm.pdf ./exams/midterm/midterm-SOLUTIONS.pdf $out/
           '';
         };
       };
       defaultPackage = packages.document;
     });
 }
-
-
-#     {
-#       templates.document = {
-#         path = ./.;
-#         description = "LaTeX document with minted support";
-#       };
-
-#       lib.latexmk = import ./build-document.nix;
-
-#       defaultTemplate = self.templates.document;
-#     } // flake-utils.lib.eachDefaultSystem (system:
-#       let
-#         pkgs = import nixpkgs { inherit system; };
-
-#         latex-packages = with pkgs; [
-#           (texlive.combine {
-#             inherit (texlive)
-#               scheme-medium
-#               exam
-#               framed
-#               titlesec
-#               cleveref
-#               multirow
-#               wrapfig
-#               tabu
-#               threeparttable
-#               threeparttablex
-#               makecell
-#               environ
-#               # biblatex
-#               # biber
-#               fvextra
-#               upquote
-#               catchfile
-#               xstring
-#               csquotes
-#               minted
-#               dejavu
-#               comment
-#               footmisc
-#               xltabular
-#               ltablex
-#               background
-#               everypage
-#               ;
-#           })
-#           which
-#           python39Packages.pygments
-#         ];
-
-#         dev-packages = with pkgs; [
-#           texlab
-#           zathura
-#           wmctrl
-#         ];
-#       in
-#       rec {
-#         devShell = pkgs.mkShell {
-#           buildInputs = [ latex-packages dev-packages ];
-#         };
-
-#         packages = flake-utils.lib.flattenTree {
-#           document = import ./build-document.nix {
-#             inherit pkgs;
-#             texlive = latex-packages;
-#             shellEscape = true;
-#             minted = true;
-#             SOURCE_DATE_EPOCH = toString self.lastModified;
-#           };
-#         };
-
-#         defaultPackage = packages.document;
-#       }
-#     );
-# }
